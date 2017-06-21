d92efa590275c67176123ba9ba1a0f9597e381b5
EdimusPrime <edimusprime@Edimus-Prime.local>
2017-06-21 18:41:34 -0400

    Finalized the JS to find quotes
    Just need to add all quotes for JS to look through.
    
    Will consider adding a 3rd category of quotes/lyrics

--- js/index.js
+++ js/index.js
@@ -11,52 +11,35 @@ function movieQuotes(){
 	$(".main").css("background-image", "url('http://img09.deviantart.net/5f7f/i/2015/052/1/a/cinema_background_by_gazlan_sahmeiy-d8j17be.jpg')");
 
 		var movQuoteList=[
-		[ ["Quote1"], ["Author1"]],
-		[ ["Quote2"], ["Author2"], ["Year"]],
+		[ ["I'm gonna make him an offer he can't refuse."], ["The Godfather"]],
+		[ ["Do or do not, There is no try"], ["Star Wars: Empire Strikes Back"]],
+		[ ["Stay on Target"], ["Star Wars: A New Hope"]],
+		[ ["Keep your friends close, but your enemies closer."], ["The Godfather II"]],
+		[ ["You want sympathy, look in the dictionary between shit and syphillis. That's where you'll find my sympathy."], ["Major Payne"]],
+		[ ["Your scientists were so preoccupied with whether they could, they didn't stop to think if they should."], ["Jurassic Park"]],
 		[ ["Quote3"], ["Author3"]],
-
+		[ ["Quote3"], ["Author3"]],
 		];
 
-		// var movQuoteList={
-		// ['id': 1, 'quote':"Quote1", 'author':"Author1"],
-		// ['id': 2, 'quote':"Quote2", 'author':"Author2", 'year':"Year"],
-		// ['id': 3, 'quote':"Quote3", 'author':"Author3"]
+		var movQuote =Math.floor(Math.random() * movQuoteList.length);
 
-		// };
-
-		
-
-		// [	['quote3'],
-		// 	['author3']
-			
-		// ]];
-
-		// var movQuote =Math.floor(Math.random() * movQuoteLength);
-		// var movQuoteLength=movQuoteList.length;
-
-// 		var lookup = {};
-// 			for (var i = 0, len = array.length; i < len; i++) {
-//     		lookup[array[i].id] = array[i];
-// }
-		// for(i=0; i<movQuoteList.length; i++)
-
-			console.log(movQuoteList[1][1])
-
-
-
-		// if (movQuote===i){
-			$('.quote').html(movQuoteList[1][0]);
-			$('.author').html(movQuoteList[1][1]);
-		// }
-
-	// alert( 'Yay Movie Quote incoming' )
+		$('.quote').html('"' + movQuoteList[movQuote][0]+ '"');
+		$('.author').html('-'+movQuoteList[movQuote][1]);
 }
 
 function vidQuotes(){
-	// for(i=0; i<movQuoteList[i].length;i)
+
 	$(".main").css("background-image", "url('http://1.bp.blogspot.com/-o0t6pL2sVIs/UUruIDZ7RaI/AAAAAAAAFn0/bS7CG8FT4Ro/s1600/console-controller-retro-desktop-wallpaper2.jpg')");
-	// var movQuote =Math.floor(Math.random() * 10);
-	// alert( 'Yay Movie Quote incoming' )
+	var vidQuoteList=[
+		[ ["Quote1"], ["Author1"]],
+		[ ["All your base are belong to us"], ["Zero Wing"]],
+		[ ["Quote3"], ["Author3"]],
+		];
+
+		var vidQuote =Math.floor(Math.random() * vidQuoteList.length);
+
+		$('.quote').html('"' + vidQuoteList[vidQuote][0]+ '"');
+		$('.author').html('-'+vidQuoteList[vidQuote][1]);
 }
 
 
